7b1c08b8788b1e1caf47816af5dcdb179d490038
Михайло Голубовський <lamerumiata@gmail.com>
2018-07-06 12:58:04 +0300

    create project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Vue from 'vue'
+  import VueResource from 'vue-resource'
+
+  Vue.use(VueResource);
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  created() {
+    this.$http.get('style.css').then(response => {
+      console.log(response.body)
+    }, response => {
+      // error callback
+    });
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
